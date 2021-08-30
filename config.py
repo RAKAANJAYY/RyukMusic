be3cbbc084e51e36ee5e083b1be1a8ed595f1751
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-08-31 02:22:03 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,28 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+ARQ_API_KEY = getenv("ARQ_API_KEY", "QFOTZM-GSZUFY-CHGHRX-TDEHOZ-ARQ")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "KGSupportgroup")
+OWNER = getenv("OWNER", "knsgnwn")
+STICKER_ID = getenv(
+    "STICKER_ID",
+    "CAACAgUAAxkBAAFF-Bdg-i8JvMgppo9DCVkFV9pPVSprzgACdwIAAsgM0VdqqiQQ6Hdw7CAE",
+)
+BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/3cf32d3eae4c81dca0792.jpg")
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ . , - : = ; !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
---
+++ main.py
@@ -0,0 +1,11 @@
+from pyrogram import Client as Bot
+
+from callsmusic import run
+from config import API_HASH, API_ID, BOT_TOKEN
+
+bot = Bot(
+    ":memory:", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="KGMusic")
+)
+
+bot.start()
+run()
